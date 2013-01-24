951c0ec41ca86fd6e652b0cc38ddb2cb6a613f19
reven <john@reven.co.nz>
2013-01-25 00:16:36 +1300

    more work done on user settings \ channel groups tab

--- NextPvrWebConsole/Scripts/controllers/usersettings/ChannelGroupsController.js
+++ NextPvrWebConsole/Scripts/controllers/usersettings/ChannelGroupsController.js
@@ -4,6 +4,7 @@ ns.ChannelGroupsController = function ($scope, $http, $rootScope) {
     "use strict";
     var self = this;
 
+    $scope.channelgroups = [];
     $scope.channels = [];
 
     $scope.refresh = function () {
@@ -14,8 +15,8 @@ ns.ChannelGroupsController = function ($scope, $http, $rootScope) {
     };
 
     $scope.add = function () {
-        $scope.channelgroups.push({
-            Oid: -1,
+        $scope.edit({
+            Oid: 0,
             Name: "",
             OrderOid: -1,
             UserOid: -1,
@@ -24,6 +25,43 @@ ns.ChannelGroupsController = function ($scope, $http, $rootScope) {
             ChannelOids: null,
             IsShared: false
         });
+    };
+
+    $scope.edit = function (item) {
+        $scope.selected = item;
+        if (!$scope.selected.Channels) {
+            $http.get('/api/channelgroups/channels/' + item.Oid + '?onlyenabled=false').success(function (data) {
+                $scope.selected.Channels = data;
+
+                // not really the angular way...
+                $('#channel-group-editor').modal();
+            });
+        } else {
+            $('#channel-group-editor').modal();
+        }
+    };
+
+    $scope.updateGroup = function () {
+        if ($scope.selected == null)
+            return;
+        if (!$scope.selected.Name || !$scope.selected.Name.trim().length)
+            return;
+        $scope.selected.Name = $scope.selected.Name.trim();
+
+        // check for duplicates
+        var duplicate = false;
+        $.each($scope.channelgroups, function (i, ele) {
+            if (ele != $scope.selected && ele.Name.toLowerCase() == $scope.selected.Name.toLowerCase())
+                duplicate = true;
+        });
+
+        if (duplicate)
+            return;
+
+        if ($scope.channelgroups.indexOf($scope.selected) < 0)
+            $scope.channelgroups.push($scope.selected);
+        // not really the angular way...
+        $('#channel-group-editor').modal('hide');
     };
 
     $scope.remove = function (item) {
